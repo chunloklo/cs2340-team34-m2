4f5e79c5ada6a5c09bb913710213b0f24c05f664
chunloklo <clo42@gatech.edu>
2017-09-07 16:40:31 -0400

    changing chunlok_lo to chunlok lo

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("mkaufer3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Chunlok_Lo");
+                  Person5 p5 = new Person5("Chunlok Lo");
                   toPrint += p5.toString("clo42");
 
         System.out.println(toPrint);
